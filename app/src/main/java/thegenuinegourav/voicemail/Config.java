7b0b3bdcd723c284f6f735d61e39cb5b1152d0cd
Hemaladani2711 <Hemaladani2711@gmail.com>
2017-10-20 16:08:36 -0700

    Initial commit

---
+++ app/src/main/java/thegenuinegourav/voicemail/Config.java
@@ -0,0 +1,6 @@
+package thegenuinegourav.voicemail;
+
+public class Config {
+    public static final String EMAIL ="yasa.sona@gmail.com";  //gmail address
+    public static final String PASSWORD ="Yasa@1212"; //password
+}
